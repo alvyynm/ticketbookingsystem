7d085be9dc58f9a46094ebec3f5f27c6d128bb41
alvyynm <alvyynm@gmail.com>
2024-02-25 13:07:34 +0300

    chore(BE): specify path variable in dotenv config options

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-require("dotenv").config();
+require("dotenv").config({ path: `${process.cwd()}/.env` });
 const port = process.env.PORT || 4000;
 
 // import routes
