065707c9c7f88ae75de003982628c9b09105e70c
alvyynm <alvyynm@gmail.com>
2024-02-25 09:23:55 +0300

    feat: create event controllers

---
+++ server/controllers/eventController.js
@@ -0,0 +1,46 @@
+const getEvents = (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    data: "event data",
+    message: "Event data fetched successfully",
+  });
+};
+
+const createEvent = (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    data: "event data",
+    message: "Event created successfully",
+  });
+};
+
+const getEventById = (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    data: "event data",
+    message: "Event data fetched successfully",
+  });
+};
+
+const updateEvent = (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    data: "event data",
+    message: "Event updated successfully",
+  });
+};
+
+const deleteEvent = (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    message: "Event deleted successfully",
+  });
+};
+
+module.exports = {
+  getEvents,
+  createEvent,
+  getEventById,
+  updateEvent,
+  deleteEvent,
+};
