4c97b19dbfdbd3eb51ea453c04f152df07ac230c
alvyynm <alvyynm@gmail.com>
2024-02-25 16:29:34 +0300

    chore(BE): update db dialect to postgres

--- server/config/config.js
+++ server/config/config.js
@@ -7,20 +7,20 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: "mysql",
+    dialect: "postgres",
   },
   test: {
     username: "root",
     password: null,
     database: "database_test",
     host: "127.0.0.1",
-    dialect: "mysql",
+    dialect: "postgres",
   },
   production: {
     username: "root",
     password: null,
     database: "database_production",
     host: "127.0.0.1",
-    dialect: "mysql",
+    dialect: "postgres",
   },
 };
