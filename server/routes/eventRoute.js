a82d3dddd44717362239007d717c23172dcf588e
alvyynm <alvyynm@gmail.com>
2024-02-25 09:37:40 +0300

    feat: create event routes

---
+++ server/routes/eventRoute.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const { body } = require("express-validator");
+const eventController = require("../controllers/eventController");
+const router = express.Router();
+
+// GET v1/events/eventId
+router.get("/events/:eventId", eventController.getEventById);
+
+// GET v1/events
+router.get("/events", eventController.getEvents);
+
+// POST v1/events
+router.post("/events", eventController.createEvent);
+
+// PUT v1/events
+router.post("/events", eventController.updateEvent);
+
+// DELETE v1/events
+router.delete("/events", eventController.deleteEvent);
+
+module.exports = router;
