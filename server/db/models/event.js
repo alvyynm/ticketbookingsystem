4fa0594e9198c081a3bcf901789e3ff4d94f8bd2
alvyynm <alvyynm@gmail.com>
2024-02-26 12:27:58 +0300

    feat(BE): define table name explicitly and create migration

--- server/db/models/event.js
+++ server/db/models/event.js
@@ -37,6 +37,8 @@ module.exports = (sequelize, DataTypes) => {
       sequelize,
       modelName: "Event",
       underscored: true,
+      // explictly declare the table name
+      tableName: "Events",
     }
   );
   return Event;
