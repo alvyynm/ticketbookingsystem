318682d56928d110ae8cfc25b7e2b6bd4ffbc6c5
alvyynm <alvyynm@gmail.com>
2024-02-26 23:18:29 +0300

    chore(BE): create middleware for checking if user is auntheticated

--- server/middleware/is-auth.js
+++ server/middleware/is-auth.js
@@ -1,1 +1,32 @@
 // middleware for checking if the user is authenticated
+const jwt = require("jsonwebtoken");
+const secret = process.env.MY_SECRET_KEY;
+
+module.exports = (req, res, next) => {
+  const authHeader = req.get("Authorization");
+
+  // check if Authorization header is included in request top determine is user is authenticated
+  if (!authHeader) {
+    const error = new Error("Not auntheticated");
+    error.statusCode = 401;
+    throw error;
+  }
+
+  const token = authHeader.split(" ")[1];
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(token, secret);
+  } catch (error) {
+    error.statusCode = 500;
+    throw error;
+  }
+
+  if (!decodedToken) {
+    const error = new Error("Not auntheticated");
+    error.statusCode = 401;
+    throw error;
+  }
+
+  req.id = decodedToken.userId;
+  next();
+};
