03292a03b1003521fe8b82baaeabff021791989c
alvyynm <alvyynm@gmail.com>
2024-02-26 08:45:52 +0300

    feat(BE): create user model

---
+++ server/db/models/user.js
@@ -0,0 +1,38 @@
+"use strict";
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init(
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      user_name: DataTypes.STRING,
+      user_email: DataTypes.STRING,
+      user_role: {
+        type: DataTypes.ENUM,
+        values: ["admin", "user"],
+      },
+      user_password: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "User",
+      underscored: true,
+    }
+  );
+  return User;
+};
