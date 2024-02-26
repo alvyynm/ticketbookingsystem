86e855b11f6c34570436bc6c7e0506414a9a0b56
alvyynm <alvyynm@gmail.com>
2024-02-26 10:27:06 +0300

    feat(BE): create Ticket model

---
+++ server/db/models/ticket.js
@@ -0,0 +1,43 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Ticket extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsTo(models.User, { foreignKey: "user_id" });
+    }
+  }
+  Ticket.init(
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      user_id: DataTypes.INTEGER,
+      event_id: DataTypes.INTEGER,
+      ticket_type: {
+        type: DataTypes.ENUM,
+        values: ["vip", "regular"],
+      },
+      reservation_date: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      ticket_price: DataTypes.FLOAT,
+      ticket_serial: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Ticket",
+      underscored: true,
+    }
+  );
+  return Ticket;
+};
