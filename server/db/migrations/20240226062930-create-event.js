71c694e2580a5e9afb2664e742f8af06e410bfcd
alvyynm <alvyynm@gmail.com>
2024-02-26 09:33:16 +0300

    feat(BE): create migration for Event model

---
+++ server/db/migrations/20240226062930-create-event.js
@@ -0,0 +1,46 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Events", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      event_name: {
+        type: Sequelize.STRING,
+      },
+      event_description: {
+        type: Sequelize.STRING,
+      },
+      start_date: {
+        type: Sequelize.DATE,
+      },
+      end_date: {
+        type: Sequelize.DATE,
+      },
+      max_attendees: {
+        type: Sequelize.INTEGER,
+      },
+      ticket_price_vip: {
+        type: Sequelize.FLOAT,
+      },
+      ticket_price_regular: {
+        type: Sequelize.FLOAT,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Events");
+  },
+};
