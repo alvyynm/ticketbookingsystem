c150fa399461d24b03da01b64ffac18d16a8d548
alvyynm <alvyynm@gmail.com>
2024-02-26 16:59:29 +0300

    fix(BE): check if `data` is true before reading the `msg` attribute

--- server/index.js
+++ server/index.js
@@ -42,9 +42,11 @@ app.use((error, req, res, next) => {
   const status = error.statusCode || 500;
   const message = error.message;
   const data = error.data;
-  res
-    .status(status)
-    .json({ status: "failed", message: data[0].msg || message, data: [] });
+  res.status(status).json({
+    status: "failed",
+    message: data ? data[0].msg : message,
+    data: [],
+  });
 });
 
 const connectDb = async () => {
