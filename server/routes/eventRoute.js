bb6ec0ae1fa25355c1b4f693647124cdb111ccf7
alvyynm <alvyynm@gmail.com>
2024-02-27 11:17:35 +0300

    feat(BE): add input validation to event update route

--- server/routes/eventRoute.js
+++ server/routes/eventRoute.js
@@ -79,7 +79,69 @@ router.post(
 );
 
 // PUT v1/events/eventId
-router.put("/events/:eventId", isAuthenticated, eventController.updateEvent);
+router.put(
+  "/events/:eventId",
+  isAuthenticated,
+  [
+    body("event_name")
+      .trim()
+      .not()
+      .isEmpty()
+      .withMessage("Event name is required"),
+    body("event_description")
+      .trim()
+      .not()
+      .isEmpty()
+      .withMessage("Event description is required")
+      .matches(/^(?=.*[A-Za-z]).{150,}$/)
+      .withMessage(
+        "Event description must be at least 150 characters, including white spaces, letters, digits, dollar sign, euro sign, or pound sign, and at least several letters"
+      ),
+    body("start_date")
+      .trim()
+      .not()
+      .isEmpty()
+      .withMessage("Event start date is required")
+      .isISO8601()
+      .withMessage("Invalid datetime format for start_date"),
+    body("end_date")
+      .optional()
+      .trim()
+      .not()
+      .isEmpty()
+      .withMessage("Event end date is required")
+      .isISO8601()
+      .withMessage("Invalid datetime format for end_date"),
+    body("max_attendees")
+      .trim()
+      .not()
+      .isEmpty()
+      .withMessage("Maximum attendees is required")
+      .isInt({ min: 10 })
+      .withMessage(
+        "Maximum attendees must be a valid integer with a minimum value of 10"
+      ),
+    body("ticket_price_vip")
+      .trim()
+      .not()
+      .isEmpty()
+      .withMessage("Ticket price for VIP is required")
+      .isNumeric({ min: 0 })
+      .withMessage(
+        "VIP ticket price must be a valid integer or float with a minimum value of 0"
+      ),
+    body("ticket_price_regular")
+      .trim()
+      .not()
+      .isEmpty()
+      .withMessage("Ticket price for regular is required")
+      .isNumeric({ min: 0 })
+      .withMessage(
+        "Regular ticket price must be a valid integer or float with a minimum value of 0"
+      ),
+  ],
+  eventController.updateEvent
+);
 
 // DELETE v1/events
 router.delete("/events", isAuthenticated, eventController.deleteEvent);
