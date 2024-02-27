be5fa8b85132a06c59c39a683ac908cf1fe6be82
alvyynm <alvyynm@gmail.com>
2024-02-28 01:28:10 +0300

    feat(BE): add ticket reservation functionality

--- server/controllers/ticketController.js
+++ server/controllers/ticketController.js
@@ -1,3 +1,6 @@
+const crypto = require("crypto");
+const { Ticket } = require("../db/models");
+
 const getTickets = (req, res, next) => {
   res.status(200).json({
     status: "success",
@@ -7,11 +10,42 @@ const getTickets = (req, res, next) => {
 };
 
 const createTicket = (req, res, next) => {
-  res.status(200).json({
-    status: "success",
-    data: "ticket data",
-    message: "Ticket created successfully",
-  });
+  const user_id = req.id;
+  const event_id = req.body.event_id;
+  const seats_reserved = req.body.seats_reserved;
+  const ticket_type = req.body.ticket_type;
+  const ticket_price = req.body.ticket_price;
+  const ticket_serial = crypto.randomUUID({ disableEntropyCache: true });
+
+  // create a new ticket
+  Ticket.create({
+    user_id: user_id,
+    event_id: event_id,
+    seats_reserved: seats_reserved,
+    ticket_type: ticket_type,
+    ticket_price: ticket_price,
+    ticket_serial: ticket_serial,
+  })
+    .then((result) => {
+      res.status(200).json({
+        status: "success",
+        message: "Ticket created successfully",
+        data: result,
+      });
+    })
+    .catch((error) => {
+      if (!error.statusCode) {
+        error.statusCode = 422;
+      }
+      error.message = "Ticket reservation failed";
+      next(error);
+    });
+
+  // res.status(200).json({
+  //   status: "success",
+  //   data: "ticket data",
+  //   message: "Ticket created successfully",
+  // });
 };
 
 const getTicketById = (req, res, next) => {
