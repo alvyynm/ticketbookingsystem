92d04a1d800a5313d88b047a2ea604d30a7c2d81
alvyynm <alvyynm@gmail.com>
2024-02-27 14:25:40 +0300

    revert: ac5a748

--- server/db/migrations/20240227100636-create-event-archive.js.js
+++ server/db/migrations/20240227100636-create-event-archive.js.js
@@ -52,6 +52,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Events");
+    await queryInterface.dropTable("EventArchives");
   },
 };
