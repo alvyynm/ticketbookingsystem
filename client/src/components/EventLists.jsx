14d67d31b9ce02d803cd4d3a233cee56cd60c63e
alvyynm <alvyynm@gmail.com>
2024-02-29 01:57:47 +0300

    feat(FE): make event cards clickable to view event details

--- client/src/components/EventLists.jsx
+++ client/src/components/EventLists.jsx
@@ -17,25 +17,27 @@ function EventLists() {
       <ul className="grid lg:grid-cols-4 md:grid-cols-2 gap-4">
         {events.map((event) => (
           <li key={event.id} className="pb-8">
-            <div className="card w-78 bg-base-100 shadow-xl">
-              <figure className="px-5 pt-5">
-                <img
-                  src={images[Math.floor(Math.random() * images.length)]}
-                  className="rounded-xl"
-                />
-              </figure>
-              <div className="card-body items-center text-center">
-                <h2 className="card-title text-white">{event.event_name}</h2>
-                <p>{Moment(event.start_date).format("D MMM YYYY, h:mm A")}</p>
-                <div className="card-actions">
-                  <Link to={`/events/${event.id}`}>
-                    <button className="btn btn-accent btn-wide font-semibold">
-                      Buy Tickets
-                    </button>
-                  </Link>
+            <Link to={`/events/${event.id}`}>
+              <div className="card w-78 bg-base-100 shadow-xl">
+                <figure className="px-5 pt-5">
+                  <img
+                    src={images[Math.floor(Math.random() * images.length)]}
+                    className="rounded-xl"
+                  />
+                </figure>
+                <div className="card-body items-center text-center">
+                  <h2 className="card-title text-white">{event.event_name}</h2>
+                  <p>{Moment(event.start_date).format("D MMM YYYY, h:mm A")}</p>
+                  <div className="card-actions">
+                    <Link to={`/events/${event.id}`}>
+                      <button className="btn btn-accent btn-wide font-semibold">
+                        Buy Tickets
+                      </button>
+                    </Link>
+                  </div>
                 </div>
               </div>
-            </div>
+            </Link>
           </li>
         ))}
       </ul>
