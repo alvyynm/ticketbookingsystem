ed7b01c0bafc9595db1b5ef5ebd648b640300310
alvyynm <alvyynm@gmail.com>
2024-02-28 05:40:44 +0300

    feat(BE): create and apply migrations for a57be21

---
+++ server/db/migrations/20240228021409-add-available_seats-to-event-table.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn("Events", "available_seats", {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      defaultValue: 1000,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn("Events", "available_seats");
+  },
+};
