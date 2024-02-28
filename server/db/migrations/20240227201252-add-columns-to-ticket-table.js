342fc7d563120f74362673a947250541bf31d126
alvyynm <alvyynm@gmail.com>
2024-02-28 05:42:58 +0300

    fix(BE): comment out migration for adding user_name, event_name and event_date to Ticket

--- server/db/migrations/20240227201252-add-columns-to-ticket-table.js
+++ server/db/migrations/20240227201252-add-columns-to-ticket-table.js
@@ -9,18 +9,18 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
-    await queryInterface.addColumn("Tickets", "user_name", {
-      type: Sequelize.STRING,
-      allowNull: false,
-    });
-    await queryInterface.addColumn("Tickets", "event_name", {
-      type: Sequelize.STRING,
-      allowNull: false,
-    });
-    await queryInterface.addColumn("Tickets", "event_date", {
-      type: Sequelize.DATE,
-      allowNull: false,
-    });
+    // await queryInterface.addColumn("Tickets", "user_name", {
+    //   type: Sequelize.STRING,
+    //   allowNull: false,
+    // });
+    // await queryInterface.addColumn("Tickets", "event_name", {
+    //   type: Sequelize.STRING,
+    //   allowNull: false,
+    // });
+    // await queryInterface.addColumn("Tickets", "event_date", {
+    //   type: Sequelize.DATE,
+    //   allowNull: false,
+    // });
   },
 
   async down(queryInterface, Sequelize) {
@@ -30,8 +30,8 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
-    await queryInterface.removeColumn("Tickets", "user_name");
-    await queryInterface.removeColumn("Tickets", "event_name");
-    await queryInterface.removeColumn("Tickets", "event_date");
+    // await queryInterface.removeColumn("Tickets", "user_name");
+    // await queryInterface.removeColumn("Tickets", "event_name");
+    // await queryInterface.removeColumn("Tickets", "event_date");
   },
 };
