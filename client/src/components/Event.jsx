13a39143a48f2989c6f7f74c289d4e98d45acdc5
alvyynm <alvyynm@gmail.com>
2024-02-28 23:38:07 +0300

    feat(FE): create Footer component

---
+++ client/src/components/Event.jsx
@@ -0,0 +1,23 @@
+import { useParams } from "react-router-dom";
+import eventsData from "../data/event.js";
+const events = eventsData;
+
+function Event() {
+  let { eventId } = useParams();
+  //   console.log("Event id", eventId);
+
+  const event = events.find((event) => event.id === +eventId);
+  //   console.log(event);
+  if (!event) {
+    return <span>The event you've requested doesn't exist.</span>;
+  }
+  const { event_name, event_description } = event;
+  return (
+    <div style={{ padding: 20 }}>
+      <h3>{event_name}</h3>
+      <p>{event_description}</p>
+    </div>
+  );
+}
+
+export default Event;
