65d035eb896d287c61724c000a10894b02fd5d92
alvyynm <alvyynm@gmail.com>
2024-02-28 15:25:21 +0300

    feat(BE): enforce five seats per event limit and several fixes

--- server/controllers/ticketController.js
+++ server/controllers/ticketController.js
@@ -1,6 +1,6 @@
 const { validationResult } = require("express-validator");
 const crypto = require("crypto");
-const { Ticket, UserEvent } = require("../db/models");
+const { Ticket, UserEvent, Event } = require("../db/models");
 
 const getTickets = (req, res, next) => {
   Ticket.findAll({
@@ -41,53 +41,127 @@ const createTicket = (req, res, next) => {
   const ticket_price = req.body.ticket_price;
   const ticket_serial = crypto.randomUUID({ disableEntropyCache: true });
 
-  let remaining_reservations;
-  // check how many tickets the user has reserved so far for the specified event
+  // 1. find event and check the available_seats for the event
+  Event.findByPk(event_id)
+    .then((result) => {
+      if (!result) {
+        res.status(404).json({
+          status: "error",
+          message: "Event not found",
+          data: [],
+        });
+      }
 
-  Ticket.findAll({
-    where: {
-      event_id: event_id,
-      user_id: user_id,
-    },
-  })
-    .then((data) => {
-      // Calculate the total seats reserved
+      const available_seats = result.available_seats;
+      // 2. calculate the number of available_seats after successful ticket reservation
+      const available_seats_after_reservation =
+        available_seats - seats_reserved;
 
-      const totalSeatsReserved = data.reduce((total, reservation) => {
-        return total + reservation.seats_reserved;
-      }, 0);
-
-      remaining_reservations = 5 - totalSeatsReserved;
-
-      if (remaining_reservations === 0 || remaining_reservations < 0) {
-        res.status(403).json({
+      if (available_seats == 0) {
+        // 3. if available_seats is 0, tell user event is fully booked
+        res.status(404).json({
           status: "failed",
-          message: "Ticket reservation limit per event exceeded",
+          message: "Event is fully booked",
           data: [],
         });
       } else {
-        if (remaining_reservations >= seats_reserved) {
-          // create a new ticket
-          Ticket.create({
-            user_id: user_id,
-            event_id: event_id,
-            seats_reserved: seats_reserved,
-            ticket_type: ticket_type,
-            ticket_price: ticket_price,
-            ticket_serial: ticket_serial,
+        // 4. if available_seats != 0:
+        // 4a. check if the number of seats the user wants to reserve are available
+        if (available_seats < seats_reserved) {
+          // 4a 1. if the user wants to reserve more than available_seats
+
+          res.status(409).json({
+            status: "error",
+            message: `Only ${available_seats} seats are currently available`,
+            data: [],
+          });
+        } else {
+          // 4a 2. if seats are available, continue to reserve
+
+          let remaining_reservations;
+
+          // 5. check how many tickets the user has reserved so far for the specified event
+
+          Ticket.findAll({
+            where: {
+              event_id: event_id,
+              user_id: user_id,
+            },
           })
-            .then((result) => {
-              // upon successful ticket reservation, update joint table
-              UserEvent.create({
-                user_id: user_id,
-                event_id: event_id,
-              }).then((userEvent) => {
-                res.status(200).json({
-                  status: "success",
-                  message: "Ticket created successfully",
-                  data: result,
+            .then((data) => {
+              // 6. Calculate the total seats reserved by the user for the specified event
+
+              const totalSeatsReserved = data.reduce((total, reservation) => {
+                return total + reservation.seats_reserved;
+              }, 0);
+
+              // Users can only reserve five seats per event
+              remaining_reservations = 5 - totalSeatsReserved;
+
+              // 7a. If remaining_reservations for the user is 0 or < 0, user can't reserve more tickets)
+              if (remaining_reservations === 0 || remaining_reservations < 0) {
+                res.status(403).json({
+                  status: "failed",
+                  message: "Ticket reservation limit per event exceeded",
+                  data: [],
                 });
-              });
+              } else {
+                // 7b. If user hasn't exceeded the reservation limit of 5 per event
+
+                if (remaining_reservations >= seats_reserved) {
+                  // 8 create a new ticket
+                  Ticket.create({
+                    user_id: user_id,
+                    event_id: event_id,
+                    seats_reserved: seats_reserved,
+                    ticket_type: ticket_type,
+                    ticket_price: ticket_price,
+                    ticket_serial: ticket_serial,
+                  })
+                    .then((result) => {
+                      // 8a. upon successful ticket reservation,
+                      // 8a 1. reduce the number of seats_available in the event by seats_reserved
+                      Event.upsert({
+                        id: event_id,
+                        available_seats: available_seats_after_reservation,
+                      }).then((updateEventResult) => {
+                        if (!updateEventResult) {
+                          // throw an error if event update failed
+                          res.status(403).json({
+                            status: "failed",
+                            message: "Something went wrong (event updating)",
+                            data: [],
+                          });
+                        }
+                        // 8a 2. update joint table
+                        UserEvent.create({
+                          user_id: user_id,
+                          event_id: event_id,
+                        }).then((userEvent) => {
+                          res.status(200).json({
+                            status: "success",
+                            message: "Ticket created successfully",
+                            data: result,
+                          });
+                        });
+                      });
+                    })
+                    .catch((error) => {
+                      if (!error.statusCode) {
+                        error.statusCode = 422;
+                      }
+                      error.message = "Ticket reservation failed";
+                      next(error);
+                    });
+                } else {
+                  // if user wants to reserve more than remaining reservation
+                  res.status(403).json({
+                    status: "failed",
+                    message: `You can only reserve ${remaining_reservations} seats for this event.`,
+                    data: result,
+                  });
+                }
+              }
             })
             .catch((error) => {
               if (!error.statusCode) {
@@ -96,13 +170,6 @@ const createTicket = (req, res, next) => {
               error.message = "Ticket reservation failed";
               next(error);
             });
-        } else {
-          // if user wants to reserve more than remaining reservation
-          res.status(403).json({
-            status: "failed",
-            message: `You can only reserve ${remaining_reservations} seats for this event.`,
-            data: result,
-          });
         }
       }
     })
