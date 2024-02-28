5e4d6ad3f081531537b4d11f712f48525b212d82
alvyynm <alvyynm@gmail.com>
2024-02-29 02:27:15 +0300

    style(FE): style event details page and add image

--- client/src/components/Event.jsx
+++ client/src/components/Event.jsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
+import Moment from "moment";
 import eventsData from "../data/event.js";
 const events = eventsData;
+import featuredImage from "../assets/party1.jpg";
 
 function Event() {
   let { eventId } = useParams();
@@ -11,11 +13,28 @@ function Event() {
   if (!event) {
     return <span>The event you've requested doesn't exist.</span>;
   }
-  const { event_name, event_description } = event;
+  const { event_name, event_description, start_date } = event;
   return (
-    <div>
-      <h3>{event_name}</h3>
-      <p>{event_description}</p>
+    <div className="min-h-64 py-8 text-left">
+      <div className="grid md:grid-cols-2 lg:gap-36 gap-8">
+        <div>
+          <p className="badge badge-accent badge-outline">
+            {Moment(start_date).format("D MMM YYYY, h:mm A")}
+          </p>
+          <h1 className="text-5xl font-bold pb-6 pt-3">{event_name}</h1>
+
+          <p>{event_description}</p>
+        </div>
+        <div>
+          <img
+            src={featuredImage}
+            className="rounded-xl"
+            width="350"
+            height="auto"
+            alt="featured event image"
+          />
+        </div>
+      </div>
     </div>
   );
 }
