fbac7f92c74e3de2460e86e94fd2add4b25338fd
alvyynm <alvyynm@gmail.com>
2024-02-29 00:42:36 +0300

    feat(FE): add contents and style to signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -3,7 +3,86 @@ import Footer from "../components/Footer";
 function Signup() {
   return (
     <>
-      <div>Signup</div>
+      <div className="h-vh">
+        <div className="hero min-h-96 bg-base-200">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="text-center lg:text-left">
+              <h1 className="text-5xl font-bold">Sign up </h1>
+              <p className="py-6">
+                Sign up to EventMania now and start buying your tickets to great
+                upcoming events. It&apos;s free
+              </p>
+            </div>
+            <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+              <form className="card-body">
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Name</span>
+                  </label>
+                  <input
+                    id="name"
+                    name="name"
+                    type="text"
+                    placeholder="full name"
+                    className="input input-bordered"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    placeholder="email"
+                    className="input input-bordered"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    placeholder="password"
+                    className="input input-bordered"
+                    required
+                  />
+                  <span className="label-text-alt text-white">
+                    Must be 8-characters long, include uppercase, lowercase
+                    letters, digits and special characters
+                  </span>
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Confirm Password</span>
+                  </label>
+                  <input
+                    id="password"
+                    type="password"
+                    placeholder="confirm password"
+                    className="input input-bordered"
+                    required
+                  />
+                  <label className="label">
+                    <span className="label-text-alt text-white">
+                      Should be identical to Password field
+                    </span>
+                  </label>
+                </div>
+                <div className="form-control mt-6">
+                  <button className="btn btn-primary">Login</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
