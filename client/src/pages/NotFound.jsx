6f11168f76ea24c7cc697c981b85d8e2848db7a0
alvyynm <alvyynm@gmail.com>
2024-02-29 00:03:02 +0300

    feat(FE): display footer below Error page

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,8 +1,11 @@
+import Footer from "../components/Footer";
+
 function NotFound() {
   return (
     <>
       <h2>404: Page Not Found</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+      <Footer />
     </>
   );
 }
