b603359ad7c05caedab6c3e5c058c0681bb7ea98
alvyynm <alvyynm@gmail.com>
2024-02-29 00:18:47 +0300

    feat(FE): add contents and style to login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,56 @@
+import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
 
 function Login() {
   return (
     <>
-      <div>Login</div>
+      <div className="h-96">
+        <div className="hero min-h-48 bg-base-200">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="text-center lg:text-left">
+              <h1 className="text-5xl font-bold">Login now!</h1>
+              <p className="py-6">
+                Sign up to EventMania now and start buying your tickets to great
+                upcoming events. It&apos;s free
+              </p>
+            </div>
+            <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+              <form className="card-body">
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="email"
+                    className="input input-bordered"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="password"
+                    className="input input-bordered"
+                    required
+                  />
+                  <label className="label">
+                    <Link to="#" className="label-text-alt link link-hover">
+                      Forgot password?
+                    </Link>
+                  </label>
+                </div>
+                <div className="form-control mt-6">
+                  <button className="btn btn-primary">Login</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
