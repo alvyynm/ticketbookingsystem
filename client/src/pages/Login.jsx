d93fbe7b249f1e320f7a481168b2b3cf47579d9f
alvyynm <alvyynm@gmail.com>
2024-02-29 15:42:13 +0300

    feat(FE): get user data when user clicks login in Login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,23 @@
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useLoginMutation } from "../slices/userApiSlice";
+import { setCredentials } from "../slices/authSlice";
+
 import Footer from "../components/Footer";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log({
+      email,
+      password,
+    });
+  };
+
   return (
     <>
       <div className="h-96">
@@ -15,7 +31,7 @@ function Login() {
               </p>
             </div>
             <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-              <form className="card-body">
+              <form onSubmit={handleSubmit} className="card-body">
                 <div className="form-control">
                   <label className="label">
                     <span className="label-text">Email</span>
@@ -24,6 +40,8 @@ function Login() {
                     id="email"
                     name="email"
                     type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                     placeholder="email"
                     className="input input-bordered"
                     required
@@ -37,6 +55,8 @@ function Login() {
                     id="password"
                     name="password"
                     type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                     placeholder="password"
                     className="input input-bordered"
                     required
@@ -48,7 +68,9 @@ function Login() {
                   </label>
                 </div>
                 <div className="form-control mt-6">
-                  <button className="btn btn-primary">Login</button>
+                  <button type="submit" className="btn btn-primary">
+                    Login
+                  </button>
                 </div>
               </form>
             </div>
