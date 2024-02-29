ecf47d486575edf4599f664f48164690eb65f401
alvyynm <alvyynm@gmail.com>
2024-02-29 14:40:21 +0300

    feat(FE): create userApiSlice for login

---
+++ client/src/slices/userApiSlice.js
@@ -0,0 +1,17 @@
+import { apiSlice } from "./apiSlice";
+
+const USER_URL = "/auth";
+
+export const userApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USER_URL}/login`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation } = userApiSlice;
