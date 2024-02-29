9d8cc366bd6471c28b653ad3a43d7821734010c5
alvyynm <alvyynm@gmail.com>
2024-03-01 00:58:59 +0300

    feat(FE): display events data in a table

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,5 +1,68 @@
+import { useGetEventsQuery } from "../slices/eventApiSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { setEvents } from "../slices/eventsSlice";
+import { useEffect } from "react";
+import Moment from "moment";
 function Admin() {
-  return <div>Admin</div>;
+  const dispatch = useDispatch();
+  const { data, isLoading } = useGetEventsQuery();
+  console.log("customers", data);
+  useEffect(() => {
+    dispatch(setEvents(data));
+  }, [data]);
+
+  const { events } = useSelector((state) => state.events);
+  console.log("events", events);
+
+  events?.map((event) => console.log(event));
+  if (!events) {
+    return (
+      <>
+        <p>No event </p>
+      </>
+    );
+  }
+  return (
+    <>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Event Name</th>
+              <th>Event Date</th>
+              <th>Max Attendees</th>
+              <th>VIP Price</th>
+              <th>Regular Price</th>
+              <th>Rem. Seats</th>
+              <th>Edit</th>
+            </tr>
+          </thead>
+          <tbody>
+            {events.map((event) => {
+              return (
+                <>
+                  <tr>
+                    <th>{event.id}</th>
+                    <td>{event.event_name}</td>
+                    <td>{Moment().calendar(event.start_date)}</td>
+                    <td>{event.max_attendees}</td>
+                    <td>{event.ticket_price_vip}</td>
+                    <td>{event.ticket_price_regular}</td>
+                    <td>{event.available_seats}</td>
+                    <td>
+                      <button>Edit</button>
+                    </td>
+                  </tr>
+                </>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
 
 export default Admin;
