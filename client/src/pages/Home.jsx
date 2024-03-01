10d6d2c04c72137bd1d4433a47b3f29db62af01c
alvyynm <alvyynm@gmail.com>
2024-03-01 11:45:22 +0300

    feat(FE): display event lists on the home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Footer from "../components/Footer";
+import EventLists from "../components/EventLists";
 
 function Home() {
   return (
     <>
       <p>Home Page</p>
+      <EventLists />
       <Footer />
     </>
   );
