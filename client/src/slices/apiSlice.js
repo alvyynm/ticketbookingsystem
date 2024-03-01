f85822882b9de0a7ae658f93ce294549f9e464c5
alvyynm <alvyynm@gmail.com>
2024-03-01 13:13:04 +0300

    revert f716f6a

--- client/src/slices/apiSlice.js
+++ client/src/slices/apiSlice.js
@@ -1,42 +1,20 @@
-import { createApi } from "@reduxjs/toolkit/query/react";
-import axios from "axios";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const API_KEY = import.meta.env.VITE_BASE_API_URL;
 
-const axiosBaseQuery =
-  ({ baseUrl } = { baseUrl: "" }) =>
-  async ({ url, method, data, params, headers }) => {
-    try {
-      const user = localStorage.getItem("userInfo");
-      const token = user?.token; // Retrieve the token from local storage
-      console.log(token);
-
-      const result = await axios({
-        url: baseUrl + url,
-        method,
-        data,
-        params,
-        headers: {
-          ...headers, // Keep existing headers
-          Authorization: token ? `Bearer ${token}` : "", // Add the token if it exists
-        },
-      });
-
-      return { data: result.data };
-    } catch (axiosError) {
-      const err = axiosError;
-      return {
-        error: {
-          status: err.response?.status,
-          data: err.response?.data || err.message,
-        },
-      };
-    }
-  };
-
-const baseQuery = axiosBaseQuery({ baseUrl: API_KEY });
+const baseQuery = fetchBaseQuery({ baseUrl: API_KEY });
 
 export const apiSlice = createApi({
   baseQuery,
+  prepareHeaders: (headers, { getState }) => {
+    const token = "help";
+
+    // If we have a token set in state, let's assume that we should be passing it.
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
+    }
+
+    return headers;
+  },
   tagTypes: ["User", "Events", "Tickets"],
   endpoints: (builder) => ({}),
 });
