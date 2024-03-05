3054a2f2f7c1dd97aefaf8f0a0c3c08b74ed0a9f
alvyynm <alvyynm@gmail.com>
2024-03-05 10:25:32 +0300

    feat(FE): create authApiSlice for login and logout

---
+++ client/src/slices/authApiSlice.js
@@ -0,0 +1,23 @@
+import { apiSlice } from "../app/api/apiSlice";
+
+const USER_URL = "/auth";
+
+export const authApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (credentials) => ({
+        url: `${USER_URL}/login`,
+        method: "POST",
+        body: { ...credentials },
+      }),
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: `${USER_URL}/logout`,
+        method: "POST",
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation, useLogoutMutation } = authApiSlice;
