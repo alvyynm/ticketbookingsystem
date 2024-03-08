8638bc653e196563e33777386278cfcaf377d99f
alvyynm <alvyynm@gmail.com>
2024-03-08 11:19:16 +0300

    fix(FE): update auth state

--- client/src/features/auth/authSlice.js
+++ client/src/features/auth/authSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   user: localStorage.getItem("user") ? localStorage.getItem("user") : null,
+  name: localStorage.getItem("name") ? localStorage.getItem("name") : null,
   token: localStorage.getItem("token") ? localStorage.getItem("token") : null,
 };
 
@@ -10,16 +11,17 @@ const authSlice = createSlice({
   initialState,
   reducers: {
     setCredentials: (state, action) => {
-      const { user, accessToken } = action.payload;
-      state.auth = { user: user, token: accessToken };
+      const { user, accessToken, name } = action.payload;
+      state.auth = { user: user, token: accessToken, name: name };
       localStorage.setItem("user", JSON.stringify(user));
       localStorage.setItem("token", JSON.stringify(accessToken));
+      localStorage.setItem("name", JSON.stringify(name));
     },
-    removeCredentials: (state, action) => {
-      state.auth.user = null;
-      state.auth.token = null;
+    removeCredentials: (state) => {
+      state.auth = { user: null, token: null, name: null };
       localStorage.removeItem("user");
       localStorage.removeItem("token");
+      localStorage.removeItem("name");
     },
   },
 });
